71cfef41205961d024c905b7ba83a14c9914da70
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2026-01-30 02:34:55 -0500

    Add component barrel exports for cleaner imports

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+// Component exports for Bid Recommendation System
+// Global Stat Solutions
+
+export { default as Header } from './Header';
+export { default as BidForm } from './BidForm';
+export { default as ResultDisplay } from './ResultDisplay';
+export { default as SegmentStats } from './SegmentStats';
+export { default as StateBenchmarks } from './StateBenchmarks';
